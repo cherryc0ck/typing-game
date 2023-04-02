c1652f07a70c6a73316f6c954999873a6187d9dc
cherryc0ck <qstar9537@naver.com>
2023-04-02 17:41:02 +0900

    add: 헤더컴포넌트 추가

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -1,7 +1,29 @@
 import React from "react";
+import styled, { css } from "styled-components";
 
 const Header = () => {
-  return <header>header</header>;
+  return (
+    <StyledHeader>
+      <h1>Typing Game</h1>
+    </StyledHeader>
+  );
 };
+
+const StyledHeader = styled.header`
+  ${({ theme }) => css`
+    display: flex;
+    justify-content: flex-start;
+    padding: 1rem 2rem;
+
+    h1 {
+      margin-bottom: 2rem;
+      background: linear-gradient(to right top, #861657, #ffa69e);
+      color: transparent;
+      font-family: "lato";
+      ${theme.typo.heavy32};
+      -webkit-background-clip: text;
+    }
+  `}
+`;
 
 export default Header;

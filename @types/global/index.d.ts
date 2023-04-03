db1bff87b12a16e8ecb7657d81ef923a8c8044e0
cherryc0ck <qstar9537@naver.com>
2023-04-03 14:42:45 +0900

    chore: clean code

---
+++ @types/global/index.d.ts
@@ -0,0 +1,11 @@
+declare module "*.jpg";
+declare module "*.png";
+declare module "*.gif";
+
+declare module "*.svg" {
+  import React = require("react");
+
+  export const ReactComponent: React.FC<React.SVGProps<SVGSVGElement>>;
+  const src: string;
+  export default src;
+}

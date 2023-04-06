beaecd2cb3fd492419ef68fd4a6fbae521475323
cherryc0ck <qstar9537@naver.com>
2023-04-06 20:42:26 +0900

    format: 코드 포맷 정리

---
+++ @types/global.d.ts
@@ -0,0 +1,9 @@
+declare module "*.png";
+declare module "*.jpg";
+declare module "*.svg" {
+  import React = require("react");
+
+  export const ReactComponent: REact.FC<React.SVGProps<SVGSVGElement>>;
+  const src: string;
+  export default src;
+}
--- eslintrc.js
+++ eslintrc.js
@@ -2,7 +2,6 @@ module.exports = {
   root: true,
   env: {
     node: true,
-    jest: true,
   },
   parser: "@typescript-eslint/parser",
   parserOptions: {
